6920dbd228fffde93f912a08c8739846b28f9209
Satchel Grant <grantsrb@gmail.com>
2017-05-28 17:10:40 -0700

    remove size assertions in run_convs()

--- style_transfer/utils/vgg16_avg.py
+++ style_transfer/utils/vgg16_avg.py
@@ -127,15 +127,11 @@ class Vgg16:
 
         # Convert RGB to BGR
         red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
-        assert red.get_shape().as_list()[1:] == [224, 224, 1]
-        assert green.get_shape().as_list()[1:] == [224, 224, 1]
-        assert blue.get_shape().as_list()[1:] == [224, 224, 1]
         bgr = tf.concat(axis=3, values=[
             blue - VGG_MEAN[0],
             green - VGG_MEAN[1],
             red - VGG_MEAN[2],
         ])
-        assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
         conv1_1 = self.conv_layer(bgr, "conv1_1")
         if layer_i == 1.1:
