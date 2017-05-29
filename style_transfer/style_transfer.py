dfa686efe98a270d5114749b4116be0fc7b9e6de
Satchel Grant <grantsrb@gmail.com>
2017-05-29 00:26:23 -0700

    improve input reception

--- style_transfer/style_transfer.py
+++ style_transfer/style_transfer.py
@@ -1,37 +1,74 @@
-# Style transfer using vgg16
-
 import numpy as np
 import tensorflow as tf
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
 from scipy.optimize import fmin_l_bfgs_b
 import utils.vgg16_avg as vgg
 from PIL import Image
 import time
 import sys
 
+# May 28th, 2017
+# Satchel Grant
+
+# Style transfer using Vgg16
+
+# The style of the style-image is transferred to the specified image
+# Pass the arguments in the command line or follow the prompt
+
 try:
     img_path = str(sys.argv[1])
     style_path = str(sys.argv[2])
+    img_size = str(sys.argv[3]).split(',')
+    img_size = (int(img_size[0]), int(img_size[1]))
     eval_conv = float(sys.argv[3])
-    styleweights = sys.argv[4].split(',')
-    styleweights = [float(x) for x in styleweights]
+    styleweights = sys.argv[4]
+    if '[' in styleweights or ']' in styleweights:
+        styleweights = styleweights[1:-1]
+    styleweights = [float(x) for x in styleweights.split(',')]
     img_weight = float(sys.argv[5])
     style_weight = float(sys.argv[6])
     n_iterations = int(sys.argv[7])
     save_path = str(sys.argv[8])
-except IndexError:
+except:
     img_path = input("Image path: ")
     style_path = input("Style path: ")
-    eval_conv = float(input("Vgg16 evaluation layer (4.1 recommended): "))
-    styleweights = input("Style layer weights (seperate by comma): ").split(',')
-    styleweights = [float(x) for x in styleweights]
-    img_weight = float(input("Image cost weight: "))
-    style_weight = float(input("Style cost weight: "))
-    n_iterations = int(input('Number of iterations: '))
-    save_path = input("Save name: ")
-
-img_size = (224,224)
+    img_h = input("Image height [300]: ")
+    if img_h == '':
+        img_h = 300
+    else:
+        img_h = int(img_h)
+    img_w = input("Image width [300]: ")
+    if img_w == '':
+        img_w = 300
+    else:
+        img_w = int(img_w)
+    eval_conv = input("Vgg16 evaluation layer [4.1]: ")
+    if eval_conv == '':
+        eval_conv = 4.1
+    else:
+        eval_conv = float(eval_conv)
+    styleweights = input("Style layer weights [0,.5,1,1]: ")
+    if styleweights == '':
+        styleweights = [0.,.5,1.,1.]
+    else:
+        styleweights = [float(x) for x in styleweights]
+    img_weight = input("Image cost weight [1]: ")
+    if img_weight == '':
+        img_weight = 1.
+    else:
+        img_weight = float(img_weight)
+    style_weight = input("Style cost weight [1]: ")
+    if style_weight == '':
+        style_weight = 1.
+    else:
+        style_weight = float(style_weight)
+    n_iterations = input('Number of iterations [10]: ')
+    if n_iterations == '':
+        n_iterations = 10
+    else:
+        n_iterations = int(n_iterations)
+    save_path = input("Save name [combined.jpg]: ")
+    if save_path == '':
+        save_path = 'combined.jpg'
 
 
 def loss_grads(gen_img, sess):
@@ -110,8 +147,7 @@ imglayers = [vmodel.run_convs(tfimg, i+.2) for i in range(1,4)]
 
 
 #### Combined
-combined_loss = img_weight*tf.reduce_mean(tf.squared_difference(img_output, trgt_const)) +\
-                style_weight*tf.reduce_sum([w*style_loss(l1,l2) for l1,l2,w in zip(stylelayers, imglayers, styleweights)])
+combined_loss = img_weight*tf.reduce_mean(tf.squared_difference(img_output, trgt_const)) + style_weight*tf.reduce_sum([w*style_loss(l1,l2) for l1,l2,w in zip(stylelayers, imglayers, styleweights)])
 
 combined_grads = tf.gradients(combined_loss, tfimg)
 
@@ -124,9 +160,6 @@ combined_img = combined_img.reshape(gen_img.shape)
 save_img = Image.fromarray((combined_img*255).astype(np.uint8))
 save_img.save(save_path)
 
-plt.imshow((combined_img*255).astype(np.uint8))
-plt.show()
-
 outimg = np.zeros([combined_img.shape[0],combined_img.shape[1]*3,3])
 outimg[:,:outimg.shape[1]//3,:] = img[:,:,:]
 outimg[:,outimg.shape[1]//3:2*outimg.shape[1]//3,:] = combined_img[:,:,:]
@@ -134,6 +167,3 @@ outimg[:,2*outimg.shape[1]//3:,:] = style[:,:,:]
 
 save_img = Image.fromarray((outimg*255).astype(np.uint8))
 save_img.save(save_path[:-4]+'_combined.jpg')
-
-
-#
